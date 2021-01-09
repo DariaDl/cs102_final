988fa2b52efbcbf6c1142b2b12bbc7a73eab9b38
Daria <D89110030406@gmail.com>
2021-01-10 00:43:06 +0300

    final

--- homework05/research/age.py
+++ homework05/research/age.py
@@ -1,3 +1,11 @@
+# pylint: disable=missing-function-docstring
+# pylint: disable=missing-module-docstring
+# pylint: disable=missing-class-docstring
+# pylint: disable=unused-argument
+# pylint: disable=unused-import
+# pylint: disable=too-many-arguments
+# pylint: disable=redefined-builtin
+
 import datetime as dt
 import statistics
 import typing as tp
@@ -14,20 +22,14 @@ def age_predict(user_id: int) -> tp.Optional[float]:
     :param user_id: Идентификатор пользователя.
     :return: Медианный возраст пользователя.
     """
-    ages = []
-    now = dt.date.today()
     friends = get_friends(user_id, fields=["bdate"]).items
+    years_of_birth = []
     for friend in friends:
         try:
             bdate = dt.datetime.strptime(friend["bdate"], "%d.%m.%Y")  # type: ignore
         except (KeyError, ValueError):
             continue
-        ages.append(
-            now.year
-            - bdate.year
-            - (now.month < bdate.month or (now.month == bdate.month and now.day < bdate.day))
-        )
-
-    if ages:
-        return statistics.median(ages)
+        years_of_birth.append(dt.datetime.now().year - bdate.year)
+    if years_of_birth:
+        return float(statistics.median(years_of_birth))
     return None
